d0c97bbdd64868678f131c1f4677f50d50beffb8
boss078 <JohnUnknownSmith@mail.ru>
2020-01-04 22:40:55 +0300

    Removed swagger stuff, added new funcs according to the task

--- src/main/java/com/boss078/jce/controllers/AdvertiserController.java
+++ src/main/java/com/boss078/jce/controllers/AdvertiserController.java
@@ -2,31 +2,72 @@ package com.boss078.jce.controllers;
 
 import com.boss078.jce.entities.Advertiser;
 import com.boss078.jce.repositories.AdvertiserRepository;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.jws.WebParam;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/advertiser/")
-@Api(value = "AdvertiserControllerAPI", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdvertiserController {
     @Autowired
     private AdvertiserRepository advertiserRepository;
 
-    @ApiOperation("Gets the Advertiser with specific id")
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "All response is OK", response = Advertiser.class)})
-    @RequestMapping("{advertiserId}")
-    public String getAdvertiser(@PathVariable String advertiserId){
-        return advertiserService
+    @GetMapping("{advertiserId}")
+    public String getAdvertiser(@PathVariable String advertiserId, Model model){
+        Optional<Advertiser> byId = advertiserRepository.findById(new Integer(advertiserId));
+        if(byId.isPresent()) {
+            model.addAttribute("advertiser", byId.get());
+            return "advertiser-index";
+        }
+        else {
+            return "error-404-index";
+        }
+    }
+
+    @GetMapping("add")
+    public String addAdvertiser(Model model){
+        model.addAttribute("advertiser", new Advertiser());
+        return "advertiser-add-index";
+    }
+
+    @PostMapping("add")
+    public String addAdvertiserDone(@ModelAttribute Advertiser advertiser, Model model){
+        advertiserRepository.save(advertiser);
+        return "advertiser-add-done-index";
+    }
+
+    @GetMapping("{advertiserId}/update")
+    public String updateAdvertiser(@PathVariable String advertiserId, Model model){
+        Optional<Advertiser> byId = advertiserRepository.findById(new Integer(advertiserId));
+        if(byId.isPresent()) {
+            model.addAttribute("advertiser", byId.get());
+            return "advertiser-update-index";
+        }
+        else {
+            return "error-404-index";
+        }
+    }
+
+    @PutMapping("{advertiserId}/add")
+    public String updateAdvertiserDone(@ModelAttribute Advertiser advertiser, Model model){
+        advertiserRepository.save(advertiser);
+        return "advertiser-add-done-index";
+    }
+
+    @GetMapping("{advertiserId}/delete")
+    public String deleteAdvertiser(@PathVariable String advertiserId, Model model){
+        Optional<Advertiser> byId = advertiserRepository.findById(new Integer(advertiserId));
+        if(byId.isPresent()) {
+            advertiserRepository.delete(byId.get());
+            return "advertiser-delete-done-index";
+        }
+        else {
+            return "error-404-index";
+        }
     }
 }
--- src/main/java/com/boss078/jce/controllers/MainController.java
+++ src/main/java/com/boss078/jce/controllers/MainController.java
@@ -1,17 +1,10 @@
 package com.boss078.jce.controllers;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Controller
-@EnableSwagger2
 public class MainController {
     @GetMapping("main")
     public String getMain(Model model){
